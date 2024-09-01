a77412cbd6064edef6fc5460d09b11d4459e36d5
H-M-Raihan-Sakib <hmraihansakib@gmail.com>
2024-09-01 21:56:13 +0600

    Create 1143 - Squared and Cubic.c

---
+++ 1143 - Squared and Cubic.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int N, i;
+
+    scanf("%d", &N);
+
+    if (N > 1 && N < 1000) {
+        for (i = 1; i <= N; i++) {
+            printf("%d %d %d\n", i, i * i, i * i * i);
+        }
+    } else {
+        printf("Invalid input. N should be between 1 and 999 (exclusive).\n");
+    }
+
+    return 0;
+}
