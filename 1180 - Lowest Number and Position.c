c98fd3719f5350fec17b8585b2ddc8a289bf84eb
H-M-Raihan-Sakib <hmraihansakib@gmail.com>
2024-09-18 21:25:21 +0600

    Create  1180 - Lowest Number and Position.c

---
+++ 1180 - Lowest Number and Position.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+
+    int N, min, pos;
+    scanf("%d", &N);
+    int X[N];
+    
+
+    for (int i = 0; i < N; i++) {
+        scanf("%d", &X[i]);
+    }
+
+    min = X[0];
+    pos = 0;
+
+    for (int i = 1; i < N; i++) {
+        if (X[i] < min) {
+            min = X[i];
+            pos = i;
+        }
+    }
+
+    printf("Menor valor: %d\n", min);
+    printf("Posicao: %d\n", pos);
+
+    return 0;
+}
