17c2f340d72dde452913ef6e66342f985c6ff03d
H-M-Raihan-Sakib <hmraihansakib@gmail.com>
2024-09-18 21:23:04 +0600

    Create  1177 - Array Fill II.c

---
+++ 1177 - Array Fill II.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int N[1000], i, T;
+
+    scanf("%d", &T);
+
+    for (i = 0; i < 1000; i++) {
+        N[i] = i % T;
+    }
+
+    for (i = 0; i < 1000; i++) {
+        printf("N[%d] = %d\n", i, N[i]);
+    }
+
+    return 0;
+}
