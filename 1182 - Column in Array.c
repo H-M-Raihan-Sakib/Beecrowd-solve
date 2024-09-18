c8862b76e8e38c0cafe35e0c27de53dffac8c928
H-M-Raihan-Sakib <hmraihansakib@gmail.com>
2024-09-18 21:28:31 +0600

    Create  1182 - Column in Array.c

---
+++ 1182 - Column in Array.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+
+    double M[12][12],sum=0,media;
+    int i,j,line,k,N;
+    char a;
+    scanf("%d",&line);
+    scanf(" %c",&a);
+    for(i=0; i<12; i++){
+        for(j=0 ; j<12 ; j++){
+            scanf("%lf",&M[i][j]);
+        }
+    }
+    for(k=0 ; k<12 ;k++){
+        sum=sum+M[k][line];
+    }
+    if(a=='S'){
+        printf("%.1lf\n",sum);
+    }
+    else if(a=='M'){
+        printf("%.1lf\n",sum/12.0);
+    }
+    
+    
+    
+    return 0;
+}
