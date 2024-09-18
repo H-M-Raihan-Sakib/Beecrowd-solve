bd561ec8603f8032adc8d5b347eb81d6d247c6f3
H-M-Raihan-Sakib <hmraihansakib@gmail.com>
2024-09-18 21:30:51 +0600

    Create  1184 - Below the Main Diagonal.c

---
+++ 1184 - Below the Main Diagonal.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main() {
+
+    double M[12][12],sum=0,media;
+    int i,j,k,l,N;
+    char O;
+
+    scanf("%c",&O);
+    for(i=0; i<12; i++){
+        for(j=0 ; j<12 ; j++){
+            scanf("%lf",&M[i][j]);
+        }
+    }
+    for(k=0 ; k<12 ;k++){
+        for(l=0; l<12;l++){
+        if(k>l){
+            sum=sum+M[k][l];
+        }
+        }
+    }
+    if(O=='S'){
+        printf("%.1lf\n",sum);
+    }
+    else if(O=='M'){
+        printf("%.1lf\n",sum/66.0);
+    }
+    
+    
+    
+    return 0;
+}
